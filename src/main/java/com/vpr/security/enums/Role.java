b3e5d690461164c72655542fa46161408285f32d
C5329198 <maryia.sinkouskaya@sap.com>
2025-05-16 10:32:05 +0300

    added enums for roles

---
+++ src/main/java/com/vpr/security/enums/Role.java
@@ -0,0 +1,21 @@
+package com.vpr.security.enums;
+
+import static com.vpr.security.enums.Authority.CREATE;
+import static com.vpr.security.enums.Authority.DELETE;
+import static com.vpr.security.enums.Authority.READ;
+import static com.vpr.security.enums.Authority.UPDATE;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.List;
+
+@RequiredArgsConstructor
+public enum Role {
+  ADMIN(List.of(CREATE.getAuthority(), READ.getAuthority(), UPDATE.getAuthority(),
+      DELETE.getAuthority())),
+  DOCTOR(List.of(CREATE.getAuthority(), READ.getAuthority(), UPDATE.getAuthority())),
+  VIEWER(List.of(READ.getAuthority()));
+
+  @Getter
+  private final List<GrantedAuthority> authorities;
+}
