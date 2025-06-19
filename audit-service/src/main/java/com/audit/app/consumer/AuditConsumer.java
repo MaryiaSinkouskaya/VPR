005f68ea36e5c38d2d2784630a9eeced2dde03fb
Maryia Sinkouskaya <masha25112000@gmail.com>
2025-06-19 15:51:11 +0300

    fix for "audit-User"

--- audit-service/src/main/java/com/audit/app/consumer/AuditConsumer.java
+++ audit-service/src/main/java/com/audit/app/consumer/AuditConsumer.java
@@ -3,15 +3,24 @@ package com.audit.app.consumer;
 import com.audit.shared.AuditEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class AuditConsumer {
 
-  @KafkaListener(topicPattern = "audit-.*", groupId = "audit-service")
-  public void listen(AuditEvent event) {
-    // Save event to DB
-    log.info("AUDIT: " + event);//todo fix delay
-  }
+    @KafkaListener(
+            topics = "audit-User",
+            groupId = "audit-service",
+            containerFactory = "kafkaListenerContainerFactory"
+    )
+    public void listenUserEvents(
+            AuditEvent event,
+            @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+            @Header(KafkaHeaders.RECEIVED_PARTITION) int partition
+    ) {
+        log.info("USER AUDIT: {} from topic: {} partition: {}", event, topic, partition);
+    }
 }
